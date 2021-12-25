f90586c13dd19cb0f03e4881943354926bfefee6
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2021-12-25 01:02:23 -0800

    updating app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
 import './App.css';
+import React, { Component } from "react";
+import {  BrowserRouter,Routes,Route } from "react-router-dom";
 import HomePage from './Pages/homepage/homepage.component';
-
+import ShopPage from './Pages/shop/shop.component';
 function App() {
   return (
-    <div >
-      <HomePage/>
-    </div>
+    <div>
+    
+      <Routes>
+        <Route exact path='/' element={<HomePage/>} />
+        <Route exact path='/shop' element={<ShopPage/>} />
+        </Routes>
+        </div>
+
+  
   );
 }
 
